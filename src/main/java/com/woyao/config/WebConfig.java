a306043be750ec6f62ee644f958b1e226d7b2d3c
jade.yang <jade.yang@activenetwork.com>
2016-10-04 11:23:33 +0800

    Init

--- src/main/java/com/woyao/config/WebConfig.java
+++ src/main/java/com/woyao/config/WebConfig.java
@@ -114,38 +114,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/mobile/").setCachePeriod(cachePeriod);
 
+		registry.addResourceHandler("/pic/**").addResourceLocations("/upload/").setCachePeriod(cachePeriod);
+
 		registry.addResourceHandler("/show/resources/**").addResourceLocations("/resources/show").setCachePeriod(cachePeriod);
 
 		registry.addResourceHandler("/admin/resources/**").addResourceLocations("/resources/admin/").setCachePeriod(cachePeriod);
 
 		registry.addResourceHandler("/shopAdmin/resources/**").addResourceLocations("/resources/shopAdmin/").setCachePeriod(cachePeriod);
 
-		// registry.addResourceHandler("/admin/resources/images/**",
-		// "/resources/images/**")
-		// .addResourceLocations("/resources/images/").setCachePeriod(cachePeriod);
-		//
-		// registry.addResourceHandler("/admin/resources/css/**",
-		// "/resources/css/**")
-		// .addResourceLocations("/resources/css/").setCacheControl(resourceLibCacheControl());
-		//
-		// registry.addResourceHandler("/admin/resources/js/**",
-		// "/resources/js/**")
-		// .addResourceLocations("/resources/js/").setCacheControl(resourceDynamicCacheControl());
-
-		// registry.addResourceHandler("/admin/resources/js/lib/**",
-		// "/resources/js/lib/**")
-		// .addResourceLocations("/resources/js/lib/").setCacheControl(resourceLibCacheControl());
-		//
-		// registry.addResourceHandler("/admin/resources/js/bb/**",
-		// "/resources/js/bb/**")
-		// .addResourceLocations("/resources/js/bb/").setCacheControl(resourceDynamicCacheControl());
-		//
-		// registry.addResourceHandler("/admin/resources/js/base/**",
-		// "/resources/js/base/**")
-		// .addResourceLocations("/resources/js/base/").setCacheControl(resourceDynamicCacheControl());
-
-		// registry.addResourceHandler("/admin/resources/data/**",
-		// "/resources/data/**").addResourceLocations(
-		// "/resources/data/");
 	}
 }
--- src/main/java/com/woyao/customer/chat/ChatUtils.java
+++ src/main/java/com/woyao/customer/chat/ChatUtils.java
@@ -1,12 +1,58 @@
 package com.woyao.customer.chat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Base64;
 import java.util.Base64.Decoder;
+import java.util.Date;
+import java.util.UUID;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public abstract class ChatUtils {
 
-	public static void decodePicString(String base64String){
+	private static final Log log = LogFactory.getLog(ChatUtils.class);
+
+	private static final String DT_PATTERN = "yyyyMMdd";
+	private static final DateFormat DF = new SimpleDateFormat(DT_PATTERN);
+	private static final String PIC_ROOT_PATH = "\\upload";
+
+	static {
+		URL url = ChatUtils.class.getClassLoader().getResource(PIC_ROOT_PATH);
+		try {
+			File root = new File(url.toURI());
+			if (!root.exists()) {
+			}
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static byte[] decodePicString(String base64String) {
 		Decoder decoder = Base64.getDecoder();
-		
+		byte[] bytes = decoder.decode(base64String);
+		return bytes;
+	}
+
+	public static String generatePicName() {
+		String dateDir = DF.format(new Date());
+
+		return UUID.randomUUID().toString();
+	}
+
+	public static String storePic(byte[] bytes, String name) throws IOException {
+		FileOutputStream write = new FileOutputStream("\\upload\\" + name);
+		try {
+			write.write(bytes);
+			return name;
+		} finally {
+			write.close();
+		}
 	}
 }
