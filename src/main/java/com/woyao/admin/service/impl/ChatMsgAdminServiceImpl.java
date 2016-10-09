3d77bf763c9f73abe09fbc4b9e05e3519e43315c
yuan <1516999592@qq.com>
2016-10-09 18:11:00 +0800

    asd

--- src/main/java/com/woyao/admin/service/impl/ChatMsgAdminServiceImpl.java
+++ src/main/java/com/woyao/admin/service/impl/ChatMsgAdminServiceImpl.java
@@ -38,7 +38,7 @@ public class ChatMsgAdminServiceImpl extends AbstractAdminService<ChatMsg, ChatM
 			criterions.add(Restrictions.eq("free",request.getFree()));
 		}
 		if (request.getShopId() != null) {
-			String hql="select SHOP_ID from ChatRoom where SHOP_ID in(select id from Shop where id="+request.getShopId()+")";
+			String hql="select shop.id from ChatRoom where shop.id in(select id from Shop where id="+request.getShopId()+")";
 			ChatRoom chatRoom=this.dao.queryUnique(hql);
 			if(chatRoom!=null){
 				request.setChatRoomId(chatRoom.getId());
