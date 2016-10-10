b2f0f5adf48a215235302c8c9e6616e98a744676
yuan <1516999592@qq.com>
2016-10-10 21:18:24 +0800

    sd

---
+++ src/main/java/com/woyao/wx/dto/UnifiedOrderRequestDTO.java
@@ -0,0 +1,212 @@
+package com.woyao.wx.dto;
+
+import java.io.Serializable;
+
+public class UnifiedOrderRequestDTO implements Serializable {
+
+	/**
+	 * 统一下单请求参数
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String appid;// 公众账号ID
+
+	private String mchId;// 商户号
+
+	private String deviceInfo;// 设备号
+
+	private String nonceStr;// 设备号
+
+	private String sign;// 签名
+
+	private String body;// 商品描述
+
+	private String detail;// 商品详情
+
+	private String attach;// 附加数据
+
+	private String outTradeNo;// 商户订单号
+
+	private String feeType;// 货币类型
+
+	private int totalFee;// 总金额
+
+	private String spbillCreateIp;// 终端IP
+
+	private String timeStart;// 交易起始时间
+
+	private String timeExpire;// 交易结束时间
+
+	private String goodsTag;// 商品标记
+
+	private String notifyUrl;// 通知地址
+
+	private String tradeType;// 交易类型
+
+	private String productId;// 商品ID
+
+	private String limitPay;// 指定支付方式
+
+	private String openid;// 用户标识
+
+	public String getAppid() {
+		return appid;
+	}
+
+	public void setAppid(String appid) {
+		this.appid = appid;
+	}
+
+	public String getMchId() {
+		return mchId;
+	}
+
+	public void setMchId(String mchId) {
+		this.mchId = mchId;
+	}
+
+	public String getDeviceInfo() {
+		return deviceInfo;
+	}
+
+	public void setDeviceInfo(String deviceInfo) {
+		this.deviceInfo = deviceInfo;
+	}
+
+	public String getNonceStr() {
+		return nonceStr;
+	}
+
+	public void setNonceStr(String nonceStr) {
+		this.nonceStr = nonceStr;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getAttach() {
+		return attach;
+	}
+
+	public void setAttach(String attach) {
+		this.attach = attach;
+	}
+
+	public String getOutTradeNo() {
+		return outTradeNo;
+	}
+
+	public void setOutTradeNo(String outTradeNo) {
+		this.outTradeNo = outTradeNo;
+	}
+
+	public String getFeeType() {
+		return feeType;
+	}
+
+	public void setFeeType(String feeType) {
+		this.feeType = feeType;
+	}
+
+	public int getTotalFee() {
+		return totalFee;
+	}
+
+	public void setTotalFee(int totalFee) {
+		this.totalFee = totalFee;
+	}
+
+	public String getSpbillCreateIp() {
+		return spbillCreateIp;
+	}
+
+	public void setSpbillCreateIp(String spbillCreateIp) {
+		this.spbillCreateIp = spbillCreateIp;
+	}
+
+	public String getTimeStart() {
+		return timeStart;
+	}
+
+	public void setTimeStart(String timeStart) {
+		this.timeStart = timeStart;
+	}
+
+	public String getTimeExpire() {
+		return timeExpire;
+	}
+
+	public void setTimeExpire(String timeExpire) {
+		this.timeExpire = timeExpire;
+	}
+
+	public String getGoodsTag() {
+		return goodsTag;
+	}
+
+	public void setGoodsTag(String goodsTag) {
+		this.goodsTag = goodsTag;
+	}
+
+	public String getNotifyUrl() {
+		return notifyUrl;
+	}
+
+	public void setNotifyUrl(String notifyUrl) {
+		this.notifyUrl = notifyUrl;
+	}
+
+	public String getTradeType() {
+		return tradeType;
+	}
+
+	public void setTradeType(String tradeType) {
+		this.tradeType = tradeType;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getLimitPay() {
+		return limitPay;
+	}
+
+	public void setLimitPay(String limitPay) {
+		this.limitPay = limitPay;
+	}
+
+	public String getOpenid() {
+		return openid;
+	}
+
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+
+}
