ec995060fd67d5d0d67c56778a647214f2e4d62a
jade.yang <jade.yang@activenetwork.com>
2016-10-25 19:08:11 +0800

    Init msg product sql.

--- config/sql/data/init_product.sql
+++ config/sql/data/init_product.sql
@@ -10,14 +10,39 @@ INSERT INTO PRODUCT
 	(id, deleted, enabled, creation_date, last_modified_date, description, name, product_type, unit_price)
 	values(-3, 0, 1, now(), now(), '付费消息，霸屏30秒', '消息-霸屏30秒', 1, 3);
 
-insert into PAID_MSG_PRODUCT_CONFIG
-	(id, deleted, enabled, creation_date, last_modified_date, hold_time, product_id)
-    values(-1, 0, 1, now(),now(), 10, -1);
+INSERT INTO PRODUCT
+	(id, deleted, enabled, creation_date, last_modified_date, description, name, product_type, unit_price)
+	values(-4, 0, 1, now(), now(), '礼物1 描述', '礼物1', 1, 1);
     
-insert into PAID_MSG_PRODUCT_CONFIG
-	(id, deleted, enabled, creation_date, last_modified_date, hold_time, product_id)
-    values(-2, 0, 1, now(),now(), 20, -2);
+INSERT INTO PRODUCT
+	(id, deleted, enabled, creation_date, last_modified_date, description, name, product_type, unit_price)
+	values(-5, 0, 1, now(), now(), '礼物2 描述', '礼物2', 1, 2);
     
-insert into PAID_MSG_PRODUCT_CONFIG
-	(id, deleted, enabled, creation_date, last_modified_date, hold_time, product_id)
-    values(-3, 0, 1, now(),now(), 30, -3);
+INSERT INTO PRODUCT
+	(id, deleted, enabled, creation_date, last_modified_date, description, name, product_type, unit_price)
+	values(-6, 0, 1, now(), now(), '礼物3 描述', '礼物3', 1, 3);
+
+	
+insert into MSG_PRODUCT
+	(id, hold_time)
+    values(-1, 10);
+    
+insert into MSG_PRODUCT
+	(id, hold_time)
+    values(-2, 20);
+    
+insert into MSG_PRODUCT
+	(id, hold_time)
+    values(-3, 30);
+
+insert into MSG_PRODUCT
+	(id, code, hold_time)
+    values(-4, 'e1', 10);
+    
+insert into MSG_PRODUCT
+	(id, code, hold_time)
+    values(-5, 'e2', 20);
+    
+insert into MSG_PRODUCT
+	(id, code, hold_time)
+    values(-6, 'e3', 30);
