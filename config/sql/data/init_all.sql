0e9ae43e17566b45132a781ce8160aa30bed9734
lzd <768851314@qq.com>
2016-10-26 22:12:25 +0800

    1

--- config/sql/data/init_all.sql
+++ config/sql/data/init_all.sql
@@ -92,8 +92,11 @@ insert into MSG_PRODUCT
 insert into MSG_PRODUCT
 	(id, code, hold_time)
     values(-6, 'e3', 30);
-    INSERT INTO `woyao`.PROFILE (id, username, type, gender, password, enabled, expired, credentials_expired, locked, creation_date, last_modified_date, deleted)
+    
+INSERT INTO `woyao`.PROFILE (id, username, type, gender, password, enabled, expired, credentials_expired, locked, creation_date, last_modified_date, deleted)
 	VALUES (-201, 'admin1', 1, 1, '44ea86d26563cd7ae457366c63a93567685c4272c608367aaa026a4f6f3313c4ec472fe37f85d884', 1, 0, 0, 0, now(), now(), 0);
+
+INSERT INTO PROFILE_ROLE (id, profile_id, role_id) VALUES (-105, -201, -103);
 
 INSERT INTO `woyao`.PICTURE (`ID`,`DELETED`,`ENABLED`,`CREATION_DATE`,`LAST_MODIFIED_DATE`,PATH, URL)
 	VALUES (-201, 0,1,now(),now(),'..', '/pic/asf-feather.png');
