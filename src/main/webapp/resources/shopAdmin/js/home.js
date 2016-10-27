714b226c72f1b87013196ea6e41531eee36fc4a3
chenjie <1005972844@qq.com>
2016-10-27 15:20:09 +0800

    6767

--- src/main/webapp/resources/shopAdmin/js/home.js
+++ src/main/webapp/resources/shopAdmin/js/home.js
@@ -84,51 +84,23 @@ define(["highcharts","exporting","dark-unica"],function(){
 		function win(){
 		    date = [];
 			price = [];
-		    pageController.shopOrderList.forEach(function(items){
-				 date.push(items.yearOrder +"/"+ items.monthOrder +"/"+ items.dayOrder);
-				 price.push(items.totalOrder);
-			 });
-		    
-		    $('#chart1').highcharts({
-    	        title: {
-    	            text:  pageController.name + "收入走势图",
-    	            x: -20 //center
-    	        },
-//    	        subtitle: {
-//    	            text: ' ',
-//    	            x: -20
-//    	        },
-    	        xAxis: {
-    	            categories:date,
-    	        },
-    	        yAxis: {
-    	            title: {
-    	                text: '金额(单位/￥)'
-    	            },
-    	            plotLines: [{
-    	                value: 0,
-    	                width: 2,
-    	                color: '#808080'
-    	            }]
-    	        },
-    	        tooltip: {
-    	            valueSuffix: '￥'
-    	        },
-    	        legend: {
-    	            layout: 'vertical',
-    	            align: 'right',
-    	            verticalAlign: 'middle',
-    	            borderWidth: 0
-    	        },
-    	        credits:{
-    	            enabled:false // 禁用版权信息
-    	       },
-    	        series: [{
-    	            name: pageController.name,
-    	            data: price
-    	        }]
-    	    });
-	    }t
+			 $.ajax({
+	 	  		  type: "post",
+	 	  		  url: "/shop/admin/order/main",
+	 	  		  success: function(data){   
+	 	  			  console.log(data);
+	 	  			  pageController.income=data;
+	 	  			  pageController.shopOrderList=data.shopOrders;
+		  	  			 pageController.shopOrderList.forEach(function(items){
+		  	  				 date.push(items.yearOrder +"/"+ items.monthOrder +"/"+ items.dayOrder);
+		  	  				 price.push(items.totalOrder);
+		  	  			 });
+	  	  				 initCharts();
+	 	  		  },
+	 	  		  dataType: 'json'
+		  	  	});
+		
+	    }
     
    
 	
