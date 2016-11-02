bfc5aa4dc03321b2d9bd5e16aab80c67c8abdf58
yuan <1516999592@qq.com>
2016-11-02 16:50:28 +0800

    test

--- src/main/java/com/woyao/admin/shop/dto/PhoneSMS.java
+++ src/main/java/com/woyao/admin/shop/dto/PhoneSMS.java
@@ -1,24 +1,23 @@
-/*package com.woyao.admin.shop.dto;
-
-import org.springframework.stereotype.Component;
-
-import com.taobao.api.ApiException;
-import com.taobao.api.DefaultTaobaoClient;
-import com.taobao.api.TaobaoClient;
-import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
-import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
+package com.woyao.admin.shop.dto;
 
 public class PhoneSMS {
 	
 	private final static String url="http://gw.api.taobao.com/router/rest";//url第三方的
-	private final static String appkey="23511293";//相当于账号
-	private final static String secret="2a6a4619ea301cb985c850c9cab812cd";//相当于密码
-	private final static String signNAme="测试";//签名名称
-	private final static String modelCode="SMS_24940402";//模块Id
+	private final static String appkey="23443133";//相当于账号
+	private final static String secret="09231554ea21627a6140206182c4cf54";//相当于密码
+	private final static String signNAme="捷报";//签名名称
+	private final static String modelCode="SMS_25100249";//模块Id
 	private final static String sMSType="normal";//类型
 	
-	public String sendSMS(String json,String tell) throws ApiException{		
-		String json="{'name':'test','num':'10','total':'3000'}";//模板里面的参数 		
+	
+	/**
+	 * ${name}，你的场子于${date}，斩获霸屏${money1}元,送出大礼${money2}元，合计：${total}元。如有疑问请于倮克达人及时联系。
+	 * @param json
+	 * @param tell
+	 * @return
+	 */
+	public String sendSMS(String json,String tell) {
+		/*String json="{'name':'test','num':'10','total':'3000'}";//模板里面的参数 		
 		TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);//建立第三方client
 		AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();//配置参数
 		req.setSmsType(sMSType);
@@ -27,7 +26,6 @@ public class PhoneSMS {
 		req.setRecNum(tell);//接收方电话
 		req.setSmsTemplateCode(modelCode);//模板Id
 		AlibabaAliqinFcSmsNumSendResponse rsp = client.execute(req);//发起请求
-		return rsp.getBody();
+*/		return null;
 	}
 }
-*/
