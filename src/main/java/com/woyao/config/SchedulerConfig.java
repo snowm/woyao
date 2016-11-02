497de48b54fdedfb8d8f2cc0b96c6b8dc5ecca7f
jade.yang <jade.yang@activenetwork.com>
2016-11-02 10:24:47 +0800

    Init

--- src/main/java/com/woyao/config/SchedulerConfig.java
+++ src/main/java/com/woyao/config/SchedulerConfig.java
@@ -1,40 +1,21 @@
 package com.woyao.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 
-import com.woyao.GlobalConfig;
-import com.woyao.customer.disruptor.LongEventProducer;
-import com.woyao.customer.service.IOrderService;
 import com.woyao.scheduler.GetWxGlobalAccessTokenTask;
 import com.woyao.scheduler.RetrySubmitOrderTask;
 import com.woyao.scheduler.SendDailyOrderReportTask;
-import com.woyao.service.GlobalAccessTokenService;
-import com.woyao.service.JsapiTicketService;
-import com.woyao.wx.WxEndpoint;
 
 @Configuration
 @Order(3)
 public class SchedulerConfig {
 
 	@Bean(name = "getWxGlobalAccessTokenTask")
-	public GetWxGlobalAccessTokenTask getWxGlobalAccessTokenTask(
-			@Qualifier("globalAccessTokenService") GlobalAccessTokenService globalTokenService,
-			@Qualifier("jsapiTicketService") JsapiTicketService jsapiTicketService, 
-			@Qualifier("wxEndpoint") WxEndpoint wxEndpoint,
-			@Qualifier("globalConfig") GlobalConfig globalConfig, 
-			@Value("${scheduler.getGlobalToken.enabled}") boolean enabled) {
+	public GetWxGlobalAccessTokenTask getWxGlobalAccessTokenTask() {
 		GetWxGlobalAccessTokenTask task = new GetWxGlobalAccessTokenTask();
 		task.setId("getWxGlobalAccessTokenTask");
-		task.setGlobalTokenService(globalTokenService);
-		task.setJsapiTicketService(jsapiTicketService);
-		task.setWxEndpoint(wxEndpoint);
-		task.setGlobalConfig(globalConfig);
-		task.setEnabled(enabled);
 		return task;
 	}
 
@@ -42,9 +23,6 @@ public class SchedulerConfig {
 	public RetrySubmitOrderTask retrySubmitOrderTask() {
 		RetrySubmitOrderTask task = new RetrySubmitOrderTask();
 		task.setId("retrySubmitOrderTask");
-//		task.setOrderService(orderService);
-//		task.setSubmitOrderProducer(submitOrderProducer);
-//		task.setBatchSize(batchSize);
 		return task;
 	}
 
