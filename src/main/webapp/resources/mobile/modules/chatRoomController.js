7d07b727053ad086888fc7776f4b13adb47d79da
lzd <768851314@qq.com>
2016-11-04 16:33:37 +0800

    show

--- src/main/webapp/resources/mobile/modules/chatRoomController.js
+++ src/main/webapp/resources/mobile/modules/chatRoomController.js
@@ -576,13 +576,19 @@ define(['jquery','avalon', 'text!./chatRoom.html','socket','swiper','wxsdk',"dom
             		if(data[i].effectCode == '' || data[i].effectCode == null){
             			data[i].picURL = "/resources/static/img/ba.png";
             		}else if(data[i].effectCode == 'e1'){
-            			data[i].picURL = "/resources/static/img/ps1.png";
+            			data[i].picURL = "/resources/static/img/e1.png";
             			pList.push(data[i]);
             		}else if(data[i].effectCode == 'e2'){
-            			data[i].picURL = "/resources/static/img/ps2.png";
+            			data[i].picURL = "/resources/static/img/s1.png";
             			pList.push(data[i]);
             		}else if(data[i].effectCode == 'e3'){
-            			data[i].picURL = "/resources/static/img/mac.png";
+            			data[i].picURL = "/resources/static/img/e3.png";
+            			pList.push(data[i]);
+            		}else if(data[i].effectCode == 'e4'){
+            			data[i].picURL = "/resources/static/img/e4.png";
+            			pList.push(data[i]);
+            		}else if(data[i].effectCode == 'e5'){
+            			data[i].picURL = "/resources/static/img/e5.png";
             			pList.push(data[i]);
             		}
             	}
